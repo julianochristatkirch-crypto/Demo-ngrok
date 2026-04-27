04279218a2b33e54ac813d2093227eb2ddcad1eb
julianochristatkirch-crypto <julianochristatkirch@gmail.com>
2026-04-28 00:36:10 +0300

    Configure middleware to trust all proxies and hosts
    Added middleware to trust all proxies and hosts for ngrok.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust all proxies for ngrok
+        $middleware->trustProxies(at: '*');
+        $middleware->trustHosts(at: ['*']);
+        
         $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
