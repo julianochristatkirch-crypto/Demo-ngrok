e29b97620f0a26d99c0187f8f00323fea15b0566
julianochristatkirch-crypto <julianochristatkirch@gmail.com>
2026-04-28 00:34:50 +0300

    Update session.php to set default secure cookie value
    Set default value for secure cookie in session config.

--- config/session.php
+++ config/session.php
@@ -169,8 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
-
+'secure' => env('SESSION_SECURE_COOKIE', false), // false pour local, true en prod
     /*
     |--------------------------------------------------------------------------
     | HTTP Access Only
